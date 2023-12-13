bdbbc01fbfa93277ffff128ea270022b11ba8afc
Ian Lourenço <ianlourenco@protonmail.com>
2023-12-13 14:24:16 -0300

    feature: comment#create

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_211_191_745) do
+ActiveRecord::Schema[7.1].define(version: 20_231_213_170_927) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
+
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.integer 'article_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id'
+  end
+
+  add_foreign_key 'comments', 'articles'
 end
